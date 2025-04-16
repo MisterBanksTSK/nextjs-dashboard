468a7594a847662aa1794675fb86faf612ae6cb0
MisterBanksTSK <mister.banks@icloud.com>
2025-04-16 15:07:31 +0700

    fix: switch from bcrypt to bcryptjs for Vercel compatibility

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -117,7 +117,7 @@
 // }
 
 
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
